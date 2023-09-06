114a6b0b8ff74375b44ff14ace2825eb1f604a7b
mvheap <miapafe@gmail.com>
2023-09-06 19:50:51 +0200

    utils file with a dictionary for mapping languages

---
+++ Utils/utils.cs
@@ -0,0 +1,13 @@
+namespace dvd_rental_api.Utils;
+public static class Utilities
+{
+    public static readonly Dictionary<string, int> languageIds = new Dictionary<string, int>()
+    {
+        {"english", 1},
+        {"italian", 2},
+        {"japanese", 3},
+        {"mandarin", 4},
+        {"french", 5},
+        {"german", 6},
+    };
+}
