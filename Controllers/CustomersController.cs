4a93378c7d5e4cf24d68f499f2ec928d3f8d4de5
mvheap <miapafe@gmail.com>
2023-09-12 10:48:49 +0200

    new endpoint of customers

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dvd_rental_api.Models;
+
+namespace dvd_rental_api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CustomersControllers : ControllerBase
+{
+    private readonly DvdrentalContext _context;
+
+    public CustomersControllers(DvdrentalContext context)
+    {
+        _context = context;
+    }
+
+    // GET customers/
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers(
+        int limit = 200
+    )
+    {
+        return await _context.Customers.OrderBy(c => c.FirstName).Take(limit).ToListAsync();
+    }
+}
