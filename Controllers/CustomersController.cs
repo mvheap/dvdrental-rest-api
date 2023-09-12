ecf4cc91bba6dd23a13a23f923f748cb71a07d25
mvheap <miapafe@gmail.com>
2023-09-12 16:10:18 +0200

    finished get by id and added email query

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -18,9 +18,31 @@ public class CustomersController : ControllerBase
     // GET customers/
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers(
-        int limit = 200
+        int limit = 200,
+        string email = null
     )
     {
-        return await _context.Customers.OrderBy(c => c.FirstName).Take(limit).ToListAsync();
+        IQueryable<Customer> currentQueried = _context.Customers;
+
+        if (email is not null)
+        {
+            currentQueried = currentQueried.Where(c => c.Email == email).Take(limit);
+        }
+
+        return await currentQueried.OrderBy(c => c.FirstName).Take(limit).ToListAsync();
+    }
+
+    // GET films/{id}
+    [HttpGet("{id:int:min(1)}")]
+    public async Task<ActionResult<Customer>> GetCustomerById(short id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+
+        if (customer is null)
+        {
+            return NotFound();
+        }
+
+        return customer;
     }
 }
